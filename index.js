7887f411a598537aa9057910421db415245bc459
若川 <lxchuan12@163.com>
2022-01-09 00:41:27 +0800

    fix: 🐛 actions 修改使用输出内容 [skip ci]

--- index.js
+++ index.js
@@ -7,17 +7,17 @@ try {
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
+  core.setOutput('time', time);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
   main().then(resArr => {
       const [err, res] = resArr;
       if(err){
-          core.setOutput(err);
+          core.setOutput('checkInResult', err);
           return;
       }
-      core.setOutput("签到结果：", res.err_msg);
+      core.setOutput('checkInResult', res.err_msg);
   });
 } catch (error) {
   core.setFailed(error.message);
