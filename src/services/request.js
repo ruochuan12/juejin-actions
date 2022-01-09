3bb78f84a4eb672c4dbc0281606f8a88b97423f2
若川 <lxchuan12@163.com>
2022-01-09 23:59:34 +0800

    docs: ✏️ 变更定时计划时间为 00:05  未登录优化 [skip ci]

--- src/services/request.js
+++ src/services/request.js
@@ -2,10 +2,25 @@ import axios from 'axios';
 import to from './await-to-js.js';
 import { headers } from '../config/index.js';
 
+export const isObject = (val) => typeof val === 'object' && val !== null;
+
 export const request = async function(options){
     console.log('调用的接口：', options);
     const lastOptions = Object.assign({}, options, { headers });
     const [err, res] = await to(axios(lastOptions));
-    const result = [err, res && res.data];
+    // res.data
+    // { err_no: 0, err_msg: 'success', data: 52055 }
+    // { err_no: 403, err_msg: 'must login', data: null }
+    let result = [err, res];
+    if(err || !isObject(res)){
+        return result;
+    }
+    const { data: { err_no : dataErrNo } } = res;
+    if(dataErrNo === 0){
+        result = [err, res && res.data];
+    }
+    if(dataErrNo === 403){
+        result = [{ ...res.data, err_msg: `${res.data.err_msg} \n 目前未登录，请检查 JUEJIN_COOKIE 是否正确` }, res.data];
+    }
     return result;
 }
