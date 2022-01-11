f7cd814da2f67e4661dd4cd8dbdd6dacc5ec4556
Sneaken <924393527@qq.com>
2022-01-11 19:16:04 +0800

    feat: 日志通过邮件发送

---
+++ src/services/email.js
@@ -0,0 +1,21 @@
+import { createTransport } from 'nodemailer';
+import { email } from '../config/index.js';
+
+export const sendEmail = async (content) => {
+    const transporter = await createTransport({
+        host,
+        port: Number(port),
+        secure: Number(port) === 465, // true for 465, false for other ports
+        auth: {
+            user: email.user, // generated ethereal user
+            pass: email.pass, // generated ethereal password
+        },
+    });
+
+    await transporter.sendMail({
+        from: `掘金 ${email.user}`, // sender address
+        to: email.to, // list of receivers
+        subject: '掘金自动签到', // Subject line
+        html: content, // html body
+    });
+};
