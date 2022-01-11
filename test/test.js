3c84757bdca7387508235186a7a778006a92b1e0
Sneaken <924393527@qq.com>
2022-01-11 23:48:55 +0800

    test: 调整测试的环境变量

--- test/test.js
+++ test/test.js
@@ -1,12 +1,14 @@
-
 import { execFile } from 'child_process';
 import { promisify } from 'util';
-// 当前路径下 cookie.js export const cookie = 'xxxx';
-import { cookie } from './cookie.js';
+import * as env from './env.js';
+
 const execFilePromise = promisify(execFile);
 
-async function main(){
-    const result = await execFilePromise('node', ['./test/index.js'], { env: { ...process.env, JUEJIN_COOKIE: cookie } });
+async function main() {
+    const result = await execFilePromise('node', ['./test/index.js'], {
+        env: { ...process.env, ...env },
+    });
     console.log(result);
 }
+
 main();
